1168260bcf1a07eecfc543bef9bbce7db2ef69bb
changwon <thswave@gmail.com>
2014-01-23 15:44:48 +0900

    euler problem 5 init

--- JavaBasicTest/src/changwon/study/java/JavaBasic.java
+++ JavaBasicTest/src/changwon/study/java/JavaBasic.java
@@ -3,5 +3,32 @@ package changwon.study.java;
 public class JavaBasic {
 	public static void main(String[] args){
 		System.out.println("hello");
+		
+		System.out.println(isPalindrome("asdfdsa"));
+		Integer a = new Integer(1);
+		
+//		System.out.println(a.);
+		StringBuilder sb = new StringBuilder();
+		sb.append("asdfdsaaa");
+		System.out.println(sb.toString());
+		System.out.println(sb.reverse());
+	}
+	
+	public static boolean isPalindrome(String str){
+		int n = str.length();
+		for (int i = 0; i < n/2 ; i++) {
+			if (str.charAt(i) != str.charAt(n-1-i)){
+				return false;
+			}
+		}
+		return true;
+	}
+	public static boolean isPalindrome(int number){
+		return reverse(number);
+	}
+
+	private static boolean reverse(int number) {
+		
+		return false;
 	}
 }
---
+++ ProjectEuler_Problem5/src/test/java/changwon/study/project/euler/test/TestEulerProblem5.java
@@ -0,0 +1,13 @@
+package changwon.study.project.euler.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestEulerProblem5 {
+	
+	@Test
+	public void test(){
+		fail("fail");
+	}
+}
