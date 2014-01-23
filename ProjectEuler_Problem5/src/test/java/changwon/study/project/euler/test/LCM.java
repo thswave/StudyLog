a4bf42e9e1ded3a867c9838766a952a09dd19c38
changwon <thswave@gmail.com>
2014-01-23 16:22:34 +0900

    euler problem 5 clear

---
+++ ProjectEuler_Problem5/src/test/java/changwon/study/project/euler/test/LCM.java
@@ -0,0 +1,31 @@
+package changwon.study.project.euler.test;
+
+public class LCM {
+
+	private int startInt;
+	private int endInt;
+
+	public LCM(int startInt, int endInt) {
+		this.startInt = startInt;
+		this.endInt = endInt;
+	}
+
+	public long getLCM() {
+		long lcm = 1;
+		while (true) {
+			for (int i = startInt; i <= endInt; i++) {
+				if ( lcm % i != 0){
+					break;
+				}
+				
+				if ( i == endInt){
+					return lcm;
+				}
+			}
+			lcm++;
+		}
+	}
+	
+	
+
+}
--- ProjectEuler_Problem5/src/test/java/changwon/study/project/euler/test/TestEulerProblem5.java
+++ ProjectEuler_Problem5/src/test/java/changwon/study/project/euler/test/TestEulerProblem5.java
@@ -4,10 +4,18 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+/**
+ * @author changwonson
+ *  1 ~ 10 사이의 어떤 수로도 나누어 떨어지는 가장 작은 수는 2520입니다.
+ *  그러면 1 ~ 20 사이의 어떤 수로도 나누어 떨어지는 가장 작은 수는 얼마입니까?
+ *
+ */
 public class TestEulerProblem5 {
 	
 	@Test
-	public void test(){
-		fail("fail");
+	public void testDivideByIntRagne(){
+		LCM lcm = new LCM(1,20);
+		assertEquals(232792560, lcm.getLCM());
+		
 	}
 }
