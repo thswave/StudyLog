d1361d2c2ec99621ee799ea54742adf93e9005c5
changwon <thswave@gmail.com>
2014-01-24 09:39:49 +0900

    euler problem 7 init

---
+++ ProjectEuler_Problem7/src/test/java/changwon/study/project/euler/test/TestEulerProblem7.java
@@ -0,0 +1,19 @@
+package changwon.study.project.euler.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author changwonson
+ * 소수를 크기 순으로 나열하면 2, 3, 5, 7, 11, 13, ... 과 같이 됩니다.
+ * 이 때 10,001번째의 소수를 구하세요.
+ *
+ */
+public class TestEulerProblem7 {
+	
+	@Test
+	public void test(){
+		fail("fail");
+	}
+}
