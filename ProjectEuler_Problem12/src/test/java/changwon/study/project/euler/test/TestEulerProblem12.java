f693f9380b43e1ddbcbe034a02384e4df99eed7e
changwon <thswave@gmail.com>
2014-01-28 12:04:31 +0900

    euler problem 12 init

---
+++ ProjectEuler_Problem12/src/test/java/changwon/study/project/euler/test/TestEulerProblem12.java
@@ -0,0 +1,17 @@
+package changwon.study.project.euler.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author changwonson
+ *
+ */
+public class TestEulerProblem12 {
+	
+	@Test
+	public void test(){
+		fail("fail");
+	}
+}
