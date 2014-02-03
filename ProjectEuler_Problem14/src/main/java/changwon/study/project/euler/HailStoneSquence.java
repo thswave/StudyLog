2e6f276ec88344c1d0631869ebde6b83d6902667
changwon <thswave@gmail.com>
2014-02-03 22:46:15 +0900

    just

--- ProjectEuler_Problem14/src/main/java/changwon/study/project/euler/HailStoneSquence.java
+++ ProjectEuler_Problem14/src/main/java/changwon/study/project/euler/HailStoneSquence.java
@@ -3,30 +3,52 @@ package changwon.study.project.euler;
 public class HailStoneSquence {
 
 	private int limitValue;
+	private int[] calcCountArr;
 
 	public HailStoneSquence(int limitValue) {
 		this.limitValue = limitValue;
+		calcCountArr = new int[limitValue * 10];
 	}
 
 	public long getMaximumCalculation() {
+		init();
 		long maxCalcCount = 0;
 		long currentCalcCount = 0;
-		for (int i = limitValue; i > 0; i--) {
-			
+//		for (int i = 2; i < limitValue; i++) {
+//			
+//			int hailstoneNumber = i;
+//			
+//			while (hailstoneNumber != 1){
+//				if (hailstoneNumber % 2 == 0){
+//					hailstoneNumber = evenCalculation(hailstoneNumber);
+//				} else {
+//					hailstoneNumber = oddCalculation(hailstoneNumber);
+//				}
+//				currentCalcCount++;
+//			}
+//			
+//			if (maxCalcCount < currentCalcCount) maxCalcCount = currentCalcCount;
+//		}
+		
+		for (int i = 2; i < limitValue; i++) {
 			int hailstoneNumber = i;
-			
 			while (hailstoneNumber != 1){
-				if (hailstoneNumber % 2 == 0){
-					hailstoneNumber = evenCalculation(hailstoneNumber);
-				} else {
-					hailstoneNumber = oddCalculation(hailstoneNumber);
-				}
-				currentCalcCount++;
+				
+//				 = currentCalcCount;
 			}
-			
-			if (maxCalcCount < currentCalcCount) maxCalcCount = currentCalcCount;
 		}
+		
+		
+		
 		return maxCalcCount;
+	}
+
+	private void init() {
+		int count = 1;
+		for (int i = 2; i < calcCountArr.length; i*=2) {
+			calcCountArr[i] = count++; 
+		}
+		
 	}
 
 	private int oddCalculation(int n) {
