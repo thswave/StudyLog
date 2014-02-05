8d00df55c6180f27395a0fbd4534b82901f550c6
changwon <thswave@gmail.com>
2014-02-05 17:43:51 +0900

    add unit test skeleton

---
+++ ProjectEuler_Problem16/src/main/java/changwon/study/project/euler/Exponential.java
@@ -0,0 +1,16 @@
+package changwon.study.project.euler;
+
+public class Exponential {
+
+	private int exponentialValue;
+
+	public Exponential(int exponentialValue) {
+		exponentialValue = exponentialValue;
+	}
+
+	public long getSumOfEachDigit() {
+		return 0L;
+	}
+	
+
+}
--- ProjectEuler_Problem16/src/test/java/changwon/study/project/euler/test/TestEulerProblem16.java
+++ ProjectEuler_Problem16/src/test/java/changwon/study/project/euler/test/TestEulerProblem16.java
@@ -4,10 +4,14 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import changwon.study.project.euler.Exponential;
+
 public class TestEulerProblem16 {
 	
 	@Test
-	public void test(){
-		fail("fail");
+	public void testExponential1000(){
+		Exponential exponential  = new Exponential(1000);
+		
+		assertEquals(0L, exponential.getSumOfEachDigit());
 	}
 }
