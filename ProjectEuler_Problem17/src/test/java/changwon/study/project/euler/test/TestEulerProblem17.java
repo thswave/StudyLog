8b363e18a8e45d8075749a193ad2d099438b4157
changwon <thswave@gmail.com>
2014-02-06 09:30:34 +0900

    euler problem 17 init

---
+++ ProjectEuler_Problem17/src/test/java/changwon/study/project/euler/test/TestEulerProblem17.java
@@ -0,0 +1,13 @@
+package changwon.study.project.euler.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestEulerProblem17 {
+	
+	@Test
+	public void test(){
+		fail("fail");
+	}
+}
